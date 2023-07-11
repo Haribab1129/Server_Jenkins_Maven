a8e0d8d6d75d12ee709e3d49d7948577e6746ea1
Haribab1129 <hariarigela70956@gmail.com>
2023-07-11 12:06:14 +0530

    comit_Third_change

--- src/main/java/Server_Jenkins_Maven/Print_String.java
+++ src/main/java/Server_Jenkins_Maven/Print_String.java
@@ -4,17 +4,19 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
 
 public class Print_String {
 	
-	public static void main(String[] args) {
+	 @Test
+	    public void mytest() {
 			System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 	        ChromeOptions options = new ChromeOptions();
 	        options.addArguments("--remote-allow-origins=*");
 		    WebDriver driver = new ChromeDriver(options);
 	        driver.manage().window().maximize();
 	        driver.get("https://www.amazon.in/");
-	        System.out.println("Print the output");
+	        System.out.println("***********************AMAZON SITE IS OPENED********************************");
 	       
 	}
 
