6072f9ad90aa4822ea100e8c984d9a2a297f980e
Haribab1129 <hariarigela70956@gmail.com>
2023-07-11 11:19:39 +0530

    comit_second_change

--- src/main/java/Server_Jenkins_Maven/Print_String.java
+++ src/main/java/Server_Jenkins_Maven/Print_String.java
@@ -1,9 +1,21 @@
 package Server_Jenkins_Maven;
 
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class Print_String {
 	
 	public static void main(String[] args) {
-		System.out.println("print the output");
+			System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+	        ChromeOptions options = new ChromeOptions();
+	        options.addArguments("--remote-allow-origins=*");
+		    WebDriver driver = new ChromeDriver(options);
+	        driver.manage().window().maximize();
+	        driver.get("https://www.amazon.in/");
+	        System.out.println("Print the output");
+	       
 	}
 
 }
